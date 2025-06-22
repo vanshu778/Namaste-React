1f26f68a479b08d1eed05f5841e34fd21d94c329
vanshu778 <vthesiya778@gmail.com>
2025-06-22 14:18:31 +0530

    Episode2 Igniting the App

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
 // ✅ Creating a single <h1> element using React.createElement
 const heading = React.createElement(
   "h1",                        // Type of element
