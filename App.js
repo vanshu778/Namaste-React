e68ff000c3d9f1325b3081633155f2c74c49c235
vanshu778 <vthesiya778@gmail.com>
2025-06-22 17:25:36 +0530

    Episode 3 Laying Foundation

--- App.js
+++ App.js
@@ -1,25 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Heading = (
-  <h1 className="root">
+const Title = () => (
+  <h1 className="root" tabIndex="5">
     Namaste React using JSX
   </h1>);
 
-
-//React Component => everything in react are component.
-//1.class based components
-//2.functional component
-
-//React Functional Component -> normal js function
-//method 1
-const HeadingComponent = () => {
-  return <h1 className="heading">Namaste React Functional Component</h1>;
-};
-//method2
-const HeadingComponent1 = () => (
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    {Title()}
+    <Title></Title>
   <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent1 />);
+root.render(<HeadingComponent  />);
