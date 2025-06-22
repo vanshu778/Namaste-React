42f1fc9fe1232eb0d843fd0d642ab05ac579f026
vanshu778 <vthesiya778@gmail.com>
2025-06-22 16:15:26 +0530

    Episode 3 Using JSX

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React.createElement => Object => HtmlElement(render)
+//React.createElement => ReactElement - Object => HtmlElement(render)
 
 const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
-
+console.log(heading);
+//using jsx
+//jsx - is not Html in js
+//jsx - Html-like or XML-like syntax
+//JSX(transpiled before it reaches tje JS Engine) - by PARCEL - give to BABEL(js package)
+//JSX => Babel transpiles it to React.createElement => ReactElement-jsObject => HtmlElement(render)
+const jsxHeading = <h1 className="root">Namaste React using JSX</h1>;
+console.log(jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
+root.render(jsxHeading);
