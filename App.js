4a6de1070123e3ff51dbfd2016a0b6bf6101cf79
vanshu778 <vthesiya778@gmail.com>
2025-06-29 14:52:12 +0530

    Episode 4 add more data

--- App.js
+++ App.js
@@ -45,6 +45,9 @@ const Header = () => {
 
 const RestaurantCard = (props) => {
   const { resData } = props;
+
+  const { cloudinaryImageId, name, cuisines, avgRating, costForTwo } =
+    resData?.info;
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
       <img
@@ -52,19 +55,266 @@ const RestaurantCard = (props) => {
         alt="res-logo"
         src={
           "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
-          resData.info.cloudinaryImageId
+          cloudinaryImageId
         }
       />
-      <h3>{resData.info.name}</h3>
-      <h4>{resData.info.cuisines.join(", ")}</h4>
-      <h4>{resData.info.avgRating} stars</h4>
-      <h4>{resData.info.costForTwo}</h4>
-      <h4>{resData.info.sla.deliveryTime} minutes</h4>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>{costForTwo}</h4>
     </div>
   );
 };
 
 const resList = [
+  {
+    info: {
+      id: "636894",
+      name: "Chinese Wok",
+      cloudinaryImageId: "e0839ff574213e6f35b3899ebf1fc597",
+      locality: "Ellis Bridge",
+      areaName: "CG Road",
+      costForTwo: "₹250 for two",
+      cuisines: ["Chinese", "Asian", "Tibetan", "Desserts"],
+      avgRating: 4.3,
+      parentId: "61955",
+      avgRatingString: "4.3",
+      totalRatingsString: "3.0K+",
+      sla: {
+        deliveryTime: 34,
+        lastMileTravel: 2,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "2.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-06-30 01:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹165 OFF",
+        subHeader: "ABOVE ₹849",
+        discountTag: "FLAT DEAL",
+        headerTypeV2: 12,
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "2.8",
+          ratingCount: "69",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-18063895-22a9-47ff-89e8-e5867dcc6d47",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/ahmedabad/chinese-wok-ellis-bridge-cg-road-rest636894",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "47589",
+      name: "Pizza Hut",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/9/468d7ec1-400c-45e1-8fe2-67237a3174cf_47589.JPG",
+      locality: "Ashram Road",
+      areaName: "Navrangpura",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "7.8K+",
+      sla: {
+        deliveryTime: 16,
+        lastMileTravel: 1.8,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "1.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-06-30 05:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹59",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "3.8",
+          ratingCount: "1.5K+",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-18063895-22a9-47ff-89e8-e5867dcc6d47",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/ahmedabad/pizza-hut-ashram-road-navrangpura-rest47589",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "81814",
+      name: "Burger King",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/213f6e83-e923-4c30-bd12-ebf12aa61ad3_81814.jpg",
+      locality: "CG Road",
+      areaName: "CG Road",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "American", "Salads", "Beverages", "Chaat"],
+      avgRating: 4.3,
+      parentId: "166",
+      avgRatingString: "4.3",
+      totalRatingsString: "33K+",
+      sla: {
+        deliveryTime: 19,
+        lastMileTravel: 1.7,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "1.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-06-30 05:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Burger.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Burger.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹110",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "4.1",
+          ratingCount: "2.5K+",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-18063895-22a9-47ff-89e8-e5867dcc6d47",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/ahmedabad/burger-king-cg-road-rest81814",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
   {
     info: {
       id: "675031",
@@ -89,10 +339,10 @@ const resList = [
       avgRatingString: "4.2",
       totalRatingsString: "4.0K+",
       sla: {
-        deliveryTime: 36,
+        deliveryTime: 25,
         lastMileTravel: 1.7,
         serviceability: "SERVICEABLE",
-        slaString: "35-40 mins",
+        slaString: "25-30 mins",
         lastMileTravelString: "1.7 km",
         iconType: "ICON_TYPE_EMPTY",
       },
@@ -159,7 +409,7 @@ const resList = [
       ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
     },
     analytics: {
-      context: "seo-data-83ccd70f-229a-40d0-94c4-9489bb03b7eb",
+      context: "seo-data-18063895-22a9-47ff-89e8-e5867dcc6d47",
     },
     cta: {
       link: "https://www.swiggy.com/city/ahmedabad/olio-the-wood-fired-pizzeria-ambawadi-ellis-bridge-rest675031",
@@ -170,29 +420,28 @@ const resList = [
   },
   {
     info: {
-      id: "765502",
-      name: "Burger Farm",
+      id: "52630",
+      name: "McDonald's",
       cloudinaryImageId:
-        "RX_THUMBNAIL/IMAGES/VENDOR/2024/5/10/213a964e-8671-4d10-aed6-4d5f0b4e2441_765502.JPG",
-      locality: "Navrangpura",
-      areaName: "Maninagar",
-      costForTwo: "₹250 for two",
-      cuisines: ["American"],
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/5/22/49e9c813-a79b-459c-a494-ef31a1644a81_52630.JPG",
+      locality: "Ellis Bridge",
+      areaName: "Ashram Road",
+      costForTwo: "₹400 for two",
+      cuisines: [],
       avgRating: 4.5,
-      veg: true,
-      parentId: "4660",
+      parentId: "630",
       avgRatingString: "4.5",
-      totalRatingsString: "1.0K+",
+      totalRatingsString: "20K+",
       sla: {
-        deliveryTime: 37,
-        lastMileTravel: 5,
+        deliveryTime: 15,
+        lastMileTravel: 1.8,
         serviceability: "SERVICEABLE",
-        slaString: "35-40 mins",
-        lastMileTravelString: "5.0 km",
+        slaString: "15-20 mins",
+        lastMileTravelString: "1.8 km",
         iconType: "ICON_TYPE_EMPTY",
       },
       availability: {
-        nextCloseTime: "2025-06-30 00:00:00",
+        nextCloseTime: "2025-06-30 04:55:00",
         opened: true,
       },
       badges: {
@@ -222,8 +471,113 @@ const resList = [
         },
       },
       aggregatedDiscountInfoV3: {
+        header: "₹80 OFF",
+        subHeader: "ABOVE ₹349",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "4.2",
+          ratingCount: "9.8K+",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-18063895-22a9-47ff-89e8-e5867dcc6d47",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/ahmedabad/mcdonalds-ellis-bridge-ashram-road-rest52630",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "37934",
+      name: "Faasos - Wraps, Rolls & Shawarma",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/28/9fc001fb-6bc1-4255-9f78-59b873c2e517_37934.JPG",
+      locality: "SV Desai Marg",
+      areaName: "CG Road RK",
+      costForTwo: "₹200 for two",
+      cuisines: [
+        "Kebabs",
+        "Fast Food",
+        "Snacks",
+        "American",
+        "Healthy Food",
+        "Desserts",
+        "Beverages",
+      ],
+      avgRating: 4.3,
+      parentId: "21809",
+      avgRatingString: "4.3",
+      totalRatingsString: "20K+",
+      sla: {
+        deliveryTime: 28,
+        lastMileTravel: 2.5,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "2.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-06-29 23:59:00",
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: "guiltfree/GF_Logo_android_3x",
+            shortDescription: "options available",
+            fontColor: "#7E808C",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "",
+                  fontColor: "#7E808C",
+                  iconId: "guiltfree/GF_Logo_android_3x",
+                  shortDescription: "options available",
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
         header: "ITEMS",
-        subHeader: "AT ₹109",
+        subHeader: "AT ₹89",
       },
       orderabilityCommunication: {
         title: {},
@@ -253,7 +607,86 @@ const resList = [
       context: "seo-data-18063895-22a9-47ff-89e8-e5867dcc6d47",
     },
     cta: {
-      link: "https://www.swiggy.com/city/ahmedabad/burger-farm-navrangpura-maninagar-rest765502",
+      link: "https://www.swiggy.com/city/ahmedabad/faasos-wraps-rolls-and-shawarma-sv-desai-marg-cg-road-rk-rest37934",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "156457",
+      name: "The Good Bowl",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/15/b9d296fa-f6f9-4cf6-a219-61efdd4c146c_156457.jpg",
+      locality: "Navrangpura",
+      areaName: "CG Road RK",
+      costForTwo: "₹400 for two",
+      cuisines: ["Biryani", "Pastas", "Punjabi", "Desserts", "Beverages"],
+      avgRating: 4.4,
+      parentId: "7918",
+      avgRatingString: "4.4",
+      totalRatingsString: "3.4K+",
+      sla: {
+        deliveryTime: 22,
+        lastMileTravel: 2.5,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "2.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-06-29 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹165 OFF",
+        subHeader: "ABOVE ₹499",
+        discountTag: "FLAT DEAL",
+        headerTypeV2: 12,
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "2.6",
+          ratingCount: "63",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-18063895-22a9-47ff-89e8-e5867dcc6d47",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/ahmedabad/the-good-bowl-navrangpura-cg-road-rk-rest156457",
       text: "RESTAURANT_MENU",
       type: "WEBLINK",
     },
@@ -268,6 +701,11 @@ const Body = () => {
       <div className="res-container">
         <RestaurantCard resData={resList[0]} />
         <RestaurantCard resData={resList[1]} />
+        <RestaurantCard resData={resList[2]} />
+        <RestaurantCard resData={resList[3]} />
+        <RestaurantCard resData={resList[4]} />
+        <RestaurantCard resData={resList[5]} />
+        <RestaurantCard resData={resList[6]} />
       </div>
     </div>
   );
