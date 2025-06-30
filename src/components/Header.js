5f0db35d63b48bdce5ef20bc9f694f6cee89a1d7
vanshu778 <vthesiya778@gmail.com>
2025-06-30 08:37:59 +0530

    Episode 5 Set the Folder Structure

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import { LOGO_URL } from "../utils/constants";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img
+          className="logo"
+          src={LOGO_URL}
+          alt="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
