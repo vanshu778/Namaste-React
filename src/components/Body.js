04743e780c0baab558297b570dfa35e94f79b118
vanshu778 <vthesiya778@gmail.com>
2025-07-01 18:41:13 +0530

    Episode 6 Apply Shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,7 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+
 const Body = () => {
   //Local State Variable - Super Powerful variable (Hooks)
   const [listOfRestaurants, setlistOfRestaurants] = useState([]);
@@ -9,55 +11,21 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.022505&lng=72.5713621&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-    
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.022505&lng=72.5713621&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
     const json = await data.json();
-    console.log(json)
-    setlistOfRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
+    console.log(json);
+    //optional chaining
+    setlistOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
-  // normal js variable
-  // let listOfRestaurants = [];
 
-  //Normal Js Varible
-  let listOfRestaurants2 = [
-    {
-      info: {
-        id: "52630",
-        name: "McDonald's",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2025/5/22/49e9c813-a79b-459c-a494-ef31a1644a81_52630.JPG",
-        costForTwo: "₹400 for two",
-        cuisines: ["Burgers", "American", "Salads", "Beverages", "Chaat"],
-        avgRating: 4.5,
-        deliveryTime: 15,
-      },
-    },
-    {
-      info: {
-        id: "52631",
-        name: "Dominos",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2025/5/22/49e9c813-a79b-459c-a494-ef31a1644a81_52630.JPG",
-        costForTwo: "₹400 for two",
-        cuisines: ["Burgers", "American", "Salads", "Beverages", "Chaat"],
-        avgRating: 3.8,
-        deliveryTime: 15,
-      },
-    },
-    {
-      info: {
-        id: "52633",
-        name: "Burger King",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2025/5/22/49e9c813-a79b-459c-a494-ef31a1644a81_52630.JPG",
-        costForTwo: "₹400 for two",
-        cuisines: ["Burgers", "American", "Salads", "Beverages", "Chaat"],
-        avgRating: 4.1,
-        deliveryTime: 15,
-      },
-    },
-  ];
-
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer/>;
+  }
   return (
     <div className="body">
       <div className="filter">
