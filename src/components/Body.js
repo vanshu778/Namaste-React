10277aceea4d16fcc4b839a0af4d32f487f94312
vanshu778 <vthesiya778@gmail.com>
2025-07-02 07:20:38 +0530

    Episode 6 ToggleButton - Login and Logout

--- src/components/Body.js
+++ src/components/Body.js
@@ -23,10 +23,7 @@ const Body = () => {
     );
   };
 
-  if (listOfRestaurants.length === 0) {
-    return <Shimmer/>;
-  }
-  return (
+  return listOfRestaurants.length === 0 ? <Shimmer/> : (
     <div className="body">
       <div className="filter">
         <button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 export const Header = () => {
+  const [btnNameReact, setbtnNameReact] = useState("Login");
+
   return (
     <div className="header">
       <div>
-        <img
-          className="logo"
-          src={LOGO_URL}
-          alt="logo"
-        />
+        <img className="logo" src={LOGO_URL} alt="logo" />
       </div>
       <div className="nav-items">
         <ul>
@@ -16,6 +15,16 @@ export const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setbtnNameReact("Logout")
+                : setbtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
