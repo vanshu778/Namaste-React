2ed28a6730b031f35d89ea3b2c560bfdb26ff66c
vanshu778 <vthesiya778@gmail.com>
2025-07-08 11:19:05 +0530

    Episode 8 Diff of state and local variable

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,10 @@
+import { useState } from "react";
 const User = ({name}) => {
+    const [count] = useState(0);
+    const [count1] = useState(1);
     return (<div className="user-card">
+        <h1>Count = {count}</h1>
+        <h1>Count1 = {count1}</h1>
         <h2>Name:{name}</h2>
         <h3>Location: Gujarat</h3>
         <h4>Contact: vanshii778</h4>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,10 +5,18 @@ class UserClass extends React.Component{
         super(props);
 
         console.log(props);
+        this.state = {
+            count: 0,
+            count1:1,
+        }
     }
     render(){
         const{name,location} = this.props;
-        return (<div className="user-card">
+        const{count,count1} = this.state;
+        return (
+        <div className="user-card">
+        <h1>Count: {count}</h1>
+        <h1>Count1: {count1}</h1>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: vanshu778</h4>
