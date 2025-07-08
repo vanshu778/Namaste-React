aed760941968239ba43a0f89550e5197f74b3d72
vanshu778 <vthesiya778@gmail.com>
2025-07-08 11:08:13 +0530

    Episode 8 ClassBased and FunctionBased Component

---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+const User = ({name}) => {
+    return (<div className="user-card">
+        <h2>Name:{name}</h2>
+        <h3>Location: Gujarat</h3>
+        <h4>Contact: vanshii778</h4>
+    </div>)
+}
+
+export default User;
