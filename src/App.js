9ceaa70fc9c8e320087e6dd8da9daea7a2b05910
vanshu778 <vthesiya778@gmail.com>
2025-07-08 16:57:07 +0530

    Episode 9 Optimizing our App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import About from "./components/About";
+// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
-// not using keys (not acceptable) <<<< index as key <<<< unique id (best practice)
+// import Grocery from "./components/Grocery";
+
+//Chunking
+//Code Splitting
+//Dynamic Bundling
+//lazy loading
+//on demand loading
+//dynamic import
+
+const Grocery = lazy(() => import("./components/Grocery"));
+
+const About = lazy(() => import("./components/About"));
 
 const AppLayout = () => {
   return (
@@ -29,7 +40,19 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <About />,
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
       },
       {
         path: "/contact",
--- src/components/Header.js
+++ src/components/Header.js
@@ -21,9 +21,13 @@ export const Header = () => {
           <li>
             <Link to="/about">About Us</Link>
           </li>
+           <li>
+            <Link to="/grocery">Grocery</Link>
+          </li>
           <li>
             <Link to="/contact">Contact Us</Link>
           </li>
+
           <li>Cart</li>
           <button
             className="login"
