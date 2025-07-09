3696c1aff2a0d464a2c316d45e5e110523249ddb
vanshu778 <vthesiya778@gmail.com>
2025-07-09 17:20:50 +0530

    Episode 11 controlled and uncontrolled

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import ItemList from "./ItemList";
 
-const RestaurantCategory = ({ data }) => {
-  const [showItems, setShowItems] = useState(false);
-  const handleClick = () => {
-    setShowItems(!showItems);
-  };
+const RestaurantCategory = ({ data,showItems, setShowIndex}) => {
+    const handleClick = () => {
+        setShowIndex();
+    }
   return (
     <div>
       {/* Header */}
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,11 +2,11 @@ import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import RestaurantCategory from "./RestaurantCategory";
-
+import { useState } from "react";
 const RestaurantMenu = () => {
   const { resId } = useParams();
   const resInfo = useRestaurantMenu(resId);
-
+  const [showIndex, setShowIndex] = useState(0); 
   if (resInfo === null) return <Shimmer />;
 
   const { name, cuisines, costForTwoMessage } =
@@ -28,10 +28,13 @@ const RestaurantMenu = () => {
         {cuisines?.join(", ")} - {costForTwoMessage}
       </p>
       {/* categories accordians */}
-      {categories.map((category) => (
+      {categories.map((category, index) => (
+        //controlled component
         <RestaurantCategory
           key={category?.card?.card.title}
           data={category?.card?.card}
+          showItems={index === showIndex ? true : false}
+          setShowIndex = {() => setShowIndex(index)}
         />
       ))}
     </div>
