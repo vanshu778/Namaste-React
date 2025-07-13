635800d611081a1dd53469dc02f4c09db14a1a36
vanshu778 <vthesiya778@gmail.com>
2025-07-14 05:29:32 +0530

    Episode 13 RestaurantCardTest Cases

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -4,7 +4,6 @@ import UserContext from "../utils/UserContext";
 
 const RestaurantCard = (props) => {
   const { resData } = props;
-
   const {loggedInUser} = useContext(UserContext)
   const { cloudinaryImageId, name, cuisines, avgRating, costForTwo } =
     resData?.info;
